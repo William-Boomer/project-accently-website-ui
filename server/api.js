b3978ab4896cd49cf822d0ee9f87e923891e0785
William Boomer Baker <boomer@boomerbaker.com>
2022-10-15 15:43:44 -0700

    Retesting

--- server/api.js
+++ server/api.js
@@ -11,12 +11,19 @@ const cookieParser = require("cookie-parser");
 const axios = require('axios');
 const requestIp = require('request-ip');
 
-mongoose.connect(process.env.MONGO_URI);
+const bizSdk = require('facebook-nodejs-business-sdk');
+const Content = bizSdk.Content;
+const CustomData = bizSdk.CustomData;
+const DeliveryCategory = bizSdk.DeliveryCategory;
+const EventRequest = bizSdk.EventRequest;
+const UserData = bizSdk.UserData;
+const ServerEvent = bizSdk.ServerEvent;
 
 const app = express();
 
 const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
 const pixel_id = process.env.FACEBOOK_PIXEL_ID;
+const api = bizSdk.FacebookAdsApi.init(access_token);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,8 +31,8 @@ app.use(cookieParser());
 app.use(requestIp.mw())
 
 //Facebook Server Side Tracking Script
-router.post("*/server-side-tracking", async (req, res) => {
-
+//router.post("*/server-side-tracking", async (req, res) => {
+/*
   let current_timestamp = Math.floor(new Date() / 1000);
 
   try {
@@ -73,10 +80,66 @@ router.post("*/server-side-tracking", async (req, res) => {
     };
 
   }
+})*/
+
+router.post("*/server-side-tracking", async (req, res) => {
+
+  let current_timestamp = Math.floor(new Date() / 1000);
+
+  try {
+    console.log("1");
+    console.log("Event Name" + req.body.eventName);
+    console.log("Event Time" + current_timestamp);
+    console.log("Event ID" + req.body.eventId);
+    console.log("Event URL" + req.body.eventUrl);
+    console.log("Event IP" + req.clientIp);
+    console.log("Event IP" + req.headers['user-agent']);
+
+    const userData = (new UserData())
+      .setClientIpAddress(req.clientIp)
+      .setClientUserAgent(req.headers['user-agent']);
+
+    const serverEvent = (new ServerEvent())
+      .setEventName(req.body.eventName)
+      .setEventTime(current_timestamp)
+      .setUserData(userData)
+      .setEventSourceUrl(req.body.eventUrl)
+      .setActionSource('website')
+      .setEventId(req.body.eventId);
+
+    const eventsData = [serverEvent];
+    const eventRequest = (new EventRequest(access_token, pixel_id))
+      .setEvents(eventsData);
+
+    const response = await eventRequest.execute()
+
+    console.log("2");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: response
+      })
+    };
+
+  } catch (err) {
+
+    console.log("3");
+    console.log("Error: " + err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: err
+      })
+    };
+
+  }
 })
 
 //Email submission endpoint
 router.post("*/submit", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URI);
+
   const existingUser = await User.findOne({ email: req.body.email });
 
   if (!existingUser) {
@@ -87,6 +150,7 @@ router.post("*/submit", async (req, res) => {
       numberOfReferrals: 0
     }).save();
   }
+  mongoose.disconnect();
   res.redirect("/early-access");
 });
 
