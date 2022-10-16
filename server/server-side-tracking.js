c07e23d7659c2ff74071e803663bf0c84dad1a6c
William Boomer Baker <boomer@boomerbaker.com>
2022-10-15 23:33:24 -0700

    Testing hashing the email

--- server/server-side-tracking.js
+++ server/server-side-tracking.js
@@ -3,6 +3,7 @@ const bizSdk = require('facebook-nodejs-business-sdk');
 const EventRequest = bizSdk.EventRequest;
 const UserData = bizSdk.UserData;
 const ServerEvent = bizSdk.ServerEvent;
+const { createHash } = require('crypto');
 
 const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
 const pixel_id = process.env.FACEBOOK_PIXEL_ID;
@@ -23,7 +24,17 @@ exports.handler = async (event, context) => {
     console.log("Event IP" + clientIp);
     console.log("Event IP" + event.headers['user-agent']);
     console.log("Email: " + data.email);
-    console.log("Email: " + data.phoneNumber);
+    console.log("Phone: " + data.phoneNumber);
+
+    if (data.email = ! null) {
+      data.email = createHash('sha256').update(data.email).digest('hex');
+      console.log("Email Updated: " + data.email);
+    }
+
+    if (data.phoneNumber = ! null) {
+      data.email = createHash('sha256').update(data.phoneNumber).digest('hex');
+      console.log("Phone Updated: " + data.phoneNumber);
+    }
 
     const userData = (new UserData())
       .setEmails([data.email])
