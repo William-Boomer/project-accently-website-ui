50ff24c2821cf357525ab0fb90237b85087ddae7
William Boomer Baker <boomer@boomerbaker.com>
2022-10-15 18:49:16 -0700

    Form should be structured correctly now

--- server/charge.js
+++ server/charge.js
@@ -2,9 +2,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
 
-  const name = event.body.split("name=")[1].split("email=")[0];
+  const name = event.body.split("name=")[1].split("&email=")[0].replaceAll('+', ' ');
   console.log("Name: " + name);
-  const email = event.body.split("email=")[1].split("stripeToken=")[0];
+  const email = event.body.split("email=")[1].split("&stripeToken=")[0];
   console.log("Name: " + email);
   const stripeToken = event.body.split("stripeToken=")[1];
   console.log("Stripe Token: " + stripeToken);
